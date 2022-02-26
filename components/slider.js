b55341608d47ba890d5446d93678bc2cba73f055
Edgar Britez <edgarfbritez@gmail.com>
2022-02-26 18:12:57 -0300

    implement testimonial Slider

---
+++ components/slider.js
@@ -0,0 +1,60 @@
+import Testimonials from "./testimonials";
+import { useEffect, useState } from "react";
+import testimonialData from "./testimonials-data.js";
+import { Box, Flex } from "@chakra-ui/react";
+
+function Dot({ onClick, bg }) {
+  // It will get the background color of the dot based on the index of the cuurenSlide
+  return (
+    <Box
+      width="10px"
+      height="10px"
+      onClick={onClick}
+      borderRadius="50%"
+      borderWidth={1}
+      borderColor="brand.BrightRed"
+      bg={bg}
+    ></Box>
+  );
+}
+
+export default function Slider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    if (currentSlide === testimonialData.length - 1) {
+      setTimeout(() => {
+        setCurrentSlide(0);
+      }, 4500);
+    }
+    const item = setTimeout(() => {
+      setCurrentSlide(currentSlide + 1);
+    }, 5000);
+    return () => clearTimeout(item);
+  }, [currentSlide]);
+
+  return (
+    <Box>
+      <Testimonials
+        key={testimonialData[currentSlide].name}
+        userData={testimonialData[currentSlide]}
+      />
+
+      <Flex justify="center" mt={4}>
+        <Flex gap={1}>
+          {testimonialData.map((user, index) => {
+            console.log("index", index);
+            return (
+              <Dot
+                key={index}
+                onClick={() => setCurrentSlide(index)}
+                // Moment aha! :)
+                bg={currentSlide === index ? "brand.BrightRed" : ""}
+              />
+            );
+          })}
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
---
+++ components/testimonials-data.js
@@ -0,0 +1,28 @@
+const testimonials = [
+  {
+    name: "Anisha Li",
+    testimonial:
+      "Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.",
+    image: "/images/avatar-anisha.png",
+  },
+  {
+    name: "Ali Bravo",
+    testimonial:
+      "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.",
+    image: "/images/avatar-ali.png",
+  },
+  {
+    name: "Richard Watts",
+    testimonial:
+      "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!",
+    image: "/images/avatar-richard.png",
+  },
+  {
+    name: "Shanai Gough",
+    testimonial:
+      "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.",
+    image: "/images/avatar-shanai.png",
+  },
+];
+
+export default testimonials;
